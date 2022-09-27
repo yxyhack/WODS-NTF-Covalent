901bdc51d6bd0f5df77e21ea7985c15eb11b7db7
yxyhack <47340733@qq.com>
2022-09-27 19:06:08 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0xe5Be78Ce3A0D4007661B3734d3Cb723F8FB3e871",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "MY WODS NFT",
 
     // 4. Set your Covalent API Key
     "api_key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
@@ -16,7 +16,7 @@ export const CONFIG = {
     "timeseries_chart": true,
 
     // 6. (Optional) Set your banner image
-    "banner_picture": "",
+    "banner_picture": "https://i.seadn.io/gae/PR10PhUcxaDBYU_Xcf0bCSlYh0Yy0AClDHxBAEGptIMsIv3VrD5WHPjljRNJqQARMvuWFOQwA8F1q_Jxg0SfEo69GRQPuq99UTS4?auto=format&w=1000",
   },
 
   // Supported networks - just for your reference
